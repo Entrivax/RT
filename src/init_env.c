ea1583e5c3160c0248e16c9603c6bed0c80a9975
Lorenzo PILOTTO <lpilotto@e3r12p14.42.fr>
2016-06-08 16:26:34 +0200

    changed sphere and cylinder matrix

--- src/init_env.c
+++ src/init_env.c
@@ -6,7 +6,7 @@
 /*   By: lpilotto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/25 14:23:19 by lpilotto          #+#    #+#             */
-/*   Updated: 2016/05/25 14:52:49 by lpilotto         ###   ########.fr       */
+/*   Updated: 2016/06/08 16:25:51 by lpilotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ static void	set_shapes_mtx(t_env *env)
 	env->sphere_mtx.mtx[0 + env->sphere_mtx.cols * 0] = 1;
 	env->sphere_mtx.mtx[1 + env->sphere_mtx.cols * 1] = 1;
 	env->sphere_mtx.mtx[2 + env->sphere_mtx.cols * 2] = 1;
+	env->sphere_mtx.mtx[3 + env->sphere_mtx.cols * 3] = -1;
 	env->plane_mtx = mtx_init(4, 4);
 	env->plane_mtx.mtx[1 + env->plane_mtx.cols * 1] = 1;
 	env->cylinder_mtx = mtx_init(4, 4);
 	env->cylinder_mtx.mtx[0 + env->cylinder_mtx.cols * 0] = 1;
 	env->cylinder_mtx.mtx[1 + env->cylinder_mtx.cols * 1] = 1;
+	env->cylinder_mtx.mtx[3 + env->cylinder_mtx.cols * 3] = -1;
 	env->cone_mtx = mtx_init(4, 4);
 	env->cone_mtx.mtx[0 + env->cone_mtx.cols * 0] = 1;
 	env->cone_mtx.mtx[1 + env->cone_mtx.cols * 1] = 1;
