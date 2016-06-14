5bc1cacd449012b14bf1152517b79be0135232fd
Lorenzo PILOTTO <lpilotto@e2r10p7.42.fr>
2016-06-14 16:21:11 +0200

    update

--- src/transform_object.c
+++ src/transform_object.c
@@ -6,7 +6,7 @@
 /*   By: lpilotto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/06 14:22:25 by lpilotto          #+#    #+#             */
-/*   Updated: 2016/06/13 15:50:46 by lpilotto         ###   ########.fr       */
+/*   Updated: 2016/06/14 15:42:58 by lpilotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	transform_object(t_obj *obj, t_tobj *tobj)
 {
-	obj->trans = mtx_product(mtx_product(tobj->scale, tobj->trans),
-		tobj->rot);
+	/*obj->trans = mtx_product(mtx_product(tobj->scale, tobj->trans),
+		tobj->rot);*/
+	obj->trans = mtx_product(tobj->trans, mtx_product(tobj->rot,
+		tobj->scale));
 	obj->i_trans = mtx_invert(obj->trans);
 }
