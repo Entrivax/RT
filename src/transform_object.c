4b97be0b2c699b63d2b30e27fba00ec566a3da9a
Lorenzo PILOTTO <lpilotto@e3r11p15.42.fr>
2016-06-24 14:58:08 +0200

    tried something

--- src/transform_object.c
+++ src/transform_object.c
@@ -6,11 +6,29 @@
 /*   By: lpilotto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/06 14:22:25 by lpilotto          #+#    #+#             */
-/*   Updated: 2016/06/20 13:37:51 by lpilotto         ###   ########.fr       */
+/*   Updated: 2016/06/24 13:59:47 by lpilotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
+
+#include <stdio.h>
+void	print_mtx(t_mtx *mtx)
+{
+	int x;
+	int y;
+
+	printf("Size: %d;%d\n", mtx->cols, mtx->rows);
+	y = -1;
+	while (++y < mtx->rows)
+	{
+		printf("[");
+		x = -1;
+		while (++x < mtx->cols)
+			printf("%lf\t", VOM(*mtx, x, y));
+		printf("]\n");
+	}
+}
 
 void	transform_object(t_obj *obj, t_tobj *tobj)
 {
@@ -18,12 +36,13 @@ void	transform_object(t_obj *obj, t_tobj *tobj)
 		tobj->rot);*/
 	tobj->ftrans = mtx_product(tobj->trans, mtx_product(tobj->rot,
 		tobj->scale));
-	tobj->i_ftrans = mtx_invert(tobj->ftrans);
 	tobj->rotxscale = mtx_product(tobj->rot, tobj->scale);
 	tobj->i_rotxscale = mtx_invert(tobj->rotxscale);
 	tobj->i_trans = mtx_invert(tobj->trans);
 	tobj->i_rot = mtx_invert(tobj->rot);
 	tobj->i_scale = mtx_invert(tobj->scale);
+	tobj->i_ftrans = mtx_product(tobj->i_trans, mtx_product(tobj->i_rot,
+		tobj->i_scale));
 	obj->trans = *tobj;
 	//obj->mtx = mtx_product(mtx_product(obj->i_trans, obj->mtx), mtx_trans(obj->i_trans));
 }
